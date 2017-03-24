832a7ffbf85690299f3c81169480f5e36cfa7dab
FrancoisTelecom <francois.5@hotmail.fr>
2017-03-24 23:02:57 +0100

    il est la le chocolat

--- StructureCv3/src/StructureCv3.c
+++ StructureCv3/src/StructureCv3.c
@@ -6,7 +6,7 @@
 struct tree {
 	int leaf;      // 0-> false, 1->true
 	int numKeys;     //how many keys does this node contain?
-	int element[DEGRE];
+	int element[DEGRE+1];
 	struct tree *stree[DEGRE+1];   //kids[i] holds nodes < keys[i] fils de l'arbre
 };
 
@@ -16,11 +16,12 @@ typedef struct tree *bTree;
 bTree initialisation(); //build Btree with 1 racine, 2 nodes and 6 leaves
 int rechercherDansNoeud(bTree tree, int val,int estpresent, bTree ssArbre);
 int search(bTree tree, int val);
+bTree returnNull(bTree tree);
 
 int main() {
 	bTree arbre;
 	arbre = initialisation();
-	printf("search : %d\n",search(arbre,7));
+	printf("search : %d\n",search(arbre,150));
 
 	free(initialisation());
 	return EXIT_SUCCESS;
@@ -31,83 +32,102 @@ bTree initialisation(){
 	bTree racine = malloc(sizeof(*racine));
 	racine->leaf=0;
 	racine->numKeys=1;
-	racine->element[0]=51;
-	racine->element[1]=NULL;
+	racine->element[0]=NULL;
+	racine->element[1]=51;
 	racine->element[2]=NULL;
 	racine->element[3]=NULL;
+	racine->element[4]=NULL;
 
 
 	bTree noeud1 = malloc(sizeof(*noeud1));
 	noeud1->leaf=0;
 	noeud1->numKeys=2;
-	noeud1->element[0]=11;
-	noeud1->element[1]=30;
-	noeud1->element[2]=NULL;
+	noeud1->element[0]=NULL;
+	noeud1->element[1]=11;
+	noeud1->element[2]=30;
 	noeud1->element[3]=NULL;
+	noeud1->element[4]=NULL;
 	racine->stree[0]=noeud1;
 
 	bTree leaf1 = malloc(sizeof(*leaf1));
+	returnNull(leaf1);
 	leaf1->leaf=1;
 	leaf1->numKeys=2;
-	leaf1->element[0]=2;
-	leaf1->element[1]=7;
-	leaf1->element[2]=NULL;
+	leaf1->element[0]=NULL;
+	leaf1->element[1]=2;
+	leaf1->element[2]=7;
 	leaf1->element[3]=NULL;
+	leaf1->element[4]=NULL;
 	noeud1->stree[0]=leaf1;
 
+
 	bTree leaf2 = malloc(sizeof(*leaf2));
+	returnNull(leaf2);
 	leaf2->leaf=1;
 	leaf2->numKeys=3;
-	leaf2->element[0]=12;
-	leaf2->element[1]=15;
-	leaf2->element[2]=22;
-	leaf2->element[3]=NULL;
+	leaf2->element[0]=NULL;
+	leaf2->element[1]=12;
+	leaf2->element[2]=15;
+	leaf2->element[3]=22;
+	leaf2->element[4]=NULL;
 	noeud1->stree[1]=leaf2;
 
+
 	bTree leaf3 = malloc(sizeof(*leaf3));
+	returnNull(leaf3);
 	leaf3->leaf=1;
 	leaf3->numKeys=2;
-	leaf3->element[0]=35;
-	leaf3->element[1]=41;
-	leaf3->element[2]=NULL;
+	leaf3->element[0]=NULL;
+	leaf3->element[1]=35;
+	leaf3->element[2]=41;
 	leaf3->element[3]=NULL;
+	leaf3->element[4]=NULL;
 	noeud1->stree[2]=leaf3;
 
 	bTree noeud2 = malloc(sizeof(*noeud2));
 	noeud2->leaf=0;
 	noeud2->numKeys=2;
-	noeud2->element[0]=66;
-	noeud2->element[1]=78;
-	noeud2->element[2]=NULL;
+	noeud2->element[0]=NULL;
+	noeud2->element[1]=66;
+	noeud2->element[2]=78;
 	noeud2->element[3]=NULL;
+	noeud2->element[4]=NULL;
 	racine->stree[1]=noeud2;
 
 	bTree leaf4 = malloc(sizeof(*leaf4));
+	returnNull(leaf4);
 	leaf4->leaf=1;
 	leaf4->numKeys=3;
-	leaf4->element[0]=53;
-	leaf4->element[1]=54;
-	leaf4->element[2]=63;
-	leaf4->element[3]=NULL;
+	leaf4->element[0]=NULL;
+	leaf4->element[1]=53;
+	leaf4->element[2]=54;
+	leaf4->element[3]=63;
+	leaf4->element[4]=NULL;
 	noeud2->stree[0]=leaf4;
 
+
 	bTree leaf5 = malloc(sizeof(*leaf5));
+	returnNull(leaf5);
 	leaf5->leaf=1;
 	leaf5->numKeys=4;
-	leaf5->element[0]=68;
-	leaf5->element[1]=69;
-	leaf5->element[2]=71;
-	leaf5->element[3]=76;
+	leaf5->element[0]=NULL;
+	leaf5->element[1]=68;
+	leaf5->element[2]=69;
+	leaf5->element[3]=71;
+	leaf5->element[4]=76;
 	noeud2->stree[1]=leaf5;
 
 	bTree leaf6 = malloc(sizeof(*leaf6));
+	returnNull(leaf6);
 	leaf6->leaf=1;
 	leaf6->numKeys=3;
-	leaf6->element[0]=79;
-	leaf6->element[1]=84;
-	leaf6->element[2]=93;
-	leaf6->element[3]=NULL;
+	leaf6->element[0]=NULL;
+	leaf6->element[1]=79;
+	leaf6->element[2]=84;
+	leaf6->element[3]=93;
+	leaf6->element[4]=NULL;
 	noeud2->stree[2]=leaf6;
+
 
 	return racine;
 }
@@ -115,21 +135,22 @@ bTree initialisation(){
 int search(bTree tree, int val){
 	int result=0;
 	int g=1,d=0,m=0;
-	bTree ssArbre;
+	//bTree ssArbre =  malloc(sizeof(*ssArbre));
+	bTree ssArbre = NULL;
 	if (tree == NULL){
 		return 0;
 	}
 	else{
-		if(val<tree->element[0]){
+		if(val<tree->element[1]){
 			return search(tree->stree[0],val);
 		}
 		else{
-			if(val>tree->element[tree->numKeys-1]){
+			if(val>tree->element[tree->numKeys]){
 				return search(tree->stree[tree->numKeys],val);
 			}
 			else{
 				//result = rechercherDansNoeud(tree, val, result, ssArbre);
-				d=tree->numKeys-1;
+				d=tree->numKeys;
 				while(g!=d){
 						m=(g+d)/2;
 						if(tree->element[m]>=val){
@@ -146,7 +167,7 @@ int search(bTree tree, int val){
 					}
 					else{
 						result=0;
-						ssArbre=tree->stree[g];
+						ssArbre=tree->stree[g-1];
 					}
 				if (result!=0){
 					return result;
@@ -179,4 +200,12 @@ int rechercherDansNoeud(bTree tree, int val, int estpresent, bTree ssArbre){
 		ssArbre=tree->stree[g];
 	}
 	return estpresent;
+}
+
+bTree returnNull(bTree tree){
+	int i=0;
+	for (i; i < DEGRE;i++) {
+		tree->stree[i] = NULL;
+	}
+	return tree;
 }
